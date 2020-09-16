c411b9b1f648b2bdd743f787c365a1be37412b11
Proudc <33157151+Proudc@users.noreply.github.com>
2020-09-16 21:10:44 +0800

    update some method files

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 sbt package
-#/home/hadoop/spark/bin/spark-submit --class "test.Test" --master spark://192.168.1.74:7077 --executor-memory 10g /home/hadoop/code/scala/target/scala-2.11/test-project_2.11-2.4.5.jar
-sudo /home/hadoop/spark/bin/spark-submit --class "src.main.scala.method.BaseZorder" --master local[*] /home/hadoop/code/scala/target/scala-2.11/test-project_2.11-2.4.5.jar
+/home/changzhihao/spark/spark/bin/spark-submit --class "src.main.scala.method.SpaceTimePar" --master spark://192.168.1.74:7077 --executor-memory 10g /home/changzhihao/sigmod/code/scala/target/scala-2.11/test-project_2.11-2.4.5.jar
+# /home/changzhihao/spark/spark/bin/spark-submit --class "src.main.scala.method.BaseZorder" --master local[*] /home/changzhihao/sigmod/code/scala/target/scala-2.11/test-project_2.11-2.4.5.jar
